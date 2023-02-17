9c77fc41a7532d7ff25af032eca35615dd492b61
ZehraSarigedikDemircioglu <z.saridem@gmail.com>
2023-02-17 13:19:03 -0500

    task 5 is done

---
+++ src/main/java/org/example/Functional.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import java.math.BigDecimal;
+import java.util.LinkedList;
+import java.util.List;
+
+public interface Functional {
+
+    LinkedList<BigDecimal> name(List<Long> parameter);
+
+}
